baa161b3ef210dea8783f239f8f409cc00b0d83e
Juan Arroyo <juanarroyobaquerizo2812@outlook.com>
2026-04-04 13:28:54 -0500

    Arquitectura base del proyecto

---
+++ src/main/java/epn/pdciae_back/PdciaeBackApplication.java
@@ -0,0 +1,13 @@
+package epn.pdciae_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdciaeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdciaeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/epn/pdciae_back/PdciaeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package epn.pdciae_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdciaeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
