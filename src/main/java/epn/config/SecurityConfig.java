c467fc4ed48d14c895d4bc9c888ea826e6bf3ea6
Juan Arroyo <juanarroyobaquerizo2812@outlook.com>
2026-04-05 18:36:48 -0500

    Solucionando problema de directorios

--- src/main/java/epn/config/SecurityConfig.java
+++ src/main/java/epn/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package epn.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@ComponentScan("epn")
 public class SecurityConfig {
 
     @Autowired
